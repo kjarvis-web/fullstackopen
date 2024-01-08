ac2aea129e05955aabf3981b656dd3314c175772
norristown <jarvasity@gmail.com>
2024-01-08 13:51:11 -0500

    exercise 5.7

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,31 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [showInfo, setShowInfo] = useState(true);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return showInfo ? (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={() => setShowInfo(!showInfo)}>view</button>
+    </div>
+  ) : (
+    <div>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setShowInfo(!showInfo)}>hide</button>
+      </div>
+      <div>{blog.url}</div>
+      <div>likes {blog.likes}</div>
+      <div>{blog.user.name}</div>
+    </div>
+  );
+};
 
 export default Blog;
