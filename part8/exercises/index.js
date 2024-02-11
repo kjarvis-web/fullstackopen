fe17362296a74a7f66a81aad2080acb80b4fd469
norristown <jarvasity@gmail.com>
2024-02-10 21:17:48 -0500

    8.4

--- part8/exercises/index.js
+++ part8/exercises/index.js
@@ -88,7 +88,7 @@ const typeDefs = `
   type Query {
     authorCount: Int!
     bookCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]
     allAuthors: [Author!]!
   }
 
@@ -110,7 +110,12 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+      return books.filter((book) => book.author === args.author)
+    },
     allAuthors: () => authors,
   },
   Author: {
